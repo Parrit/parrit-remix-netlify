9845142efba1169e8998c0cc89711549843a0994
Pinwheeler <anthonydreessen@gmail.com>
2024-12-27 23:13:19 -0800

    basic error handling

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from "@remix-run/react";
 
 import "~/styles/global.css";
@@ -32,6 +33,38 @@ export default function App() {
       <body>
         <Outlet />
         <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  console.error(error);
+  return (
+    <html>
+      <head>
+        <title>Oh no!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div className="p-10 bg-yellow-100">
+          <h1>RAWWWK! Polly's in trouble!</h1>
+          <p>
+            We apologize for the inconvenience. Our team is working to fix the
+            issue.
+          </p>
+          <button
+            className="button-red px-10"
+            onClick={() => window.location.replace("/logout")}
+          >
+            Start over
+          </button>
+          {/* Optionally, display some debugging information for developers */}
+          <pre>{`${error}`}</pre>
+        </div>
         <Scripts />
       </body>
     </html>
