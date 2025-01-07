ede4c0549496a5e9498045a835bd39a842d5d6bd
Pinwheeler <anthonydreessen@gmail.com>
2025-01-06 22:51:35 -0800

    publish sentry environment

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -6,6 +6,7 @@ import { hydrateRoot } from "react-dom/client";
 Sentry.init({
   dsn: "https://c8a47fcd86fce0c2b5913396f6b08533@o4508546853830656.ingest.us.sentry.io/4508546855272448",
   tracesSampleRate: 1,
+  environment: process.env.SENTRY_ENV,
 
   integrations: [
     Sentry.browserTracingIntegration({
