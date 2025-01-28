4f97c4f096367654a85515fe3bb9808f5288d8e3
Pinwheeler <anthonydreessen@gmail.com>
2025-01-27 18:24:39 -0800

    Ensure sentry startup
    - use an SVG for copyright

--- app/routes/project.$projectId/components/ui/Footer.tsx
+++ app/routes/project.$projectId/components/ui/Footer.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "~/styles/footer.css";
+import copyright from "~/styles/images/copyright.svg";
 
 export const Footer: React.FC = () => {
   return (
     <footer>
-      <div className="fake-copyright">© Parrit 2025</div>
+      <div className="fake-copyright flex items-center space-x-1">
+        <img className="h-2 w-2 inline-block" src={copyright} />
+        <span>Parrit 2025</span>
+      </div>
       <div className="footer-links">
         <a
           target="_blank"
